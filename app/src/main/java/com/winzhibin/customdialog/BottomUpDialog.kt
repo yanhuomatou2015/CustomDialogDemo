cd62ff14294fdf041c566899d1e128462c2520c2
lieri <lieri0318>
2020-11-01 00:01:34 +0800

    this is custom dialog

---
+++ app/src/main/java/com/winzhibin/customdialog/BottomUpDialog.kt
@@ -0,0 +1,31 @@
+package com.winzhibin.customdialog
+
+import android.app.Dialog
+import android.content.Context
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.Button
+import kotlinx.android.synthetic.main.dialog_layout.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
+
+class BottomUpDialog :Dialog {
+
+    private val mBtncancle: Button by lazy {
+        btn_cancel
+    }
+
+    //一个参数的构造方法调用两个参数的构造方法
+    constructor(context: Context?): this(context,0)
+    constructor(context: Context?,themeResId:Int):super(context!!,R.style.bottom_dialog){//引入自定义样式
+        //关联自定义dialog布局
+        setContentView(R.layout.dialog_layout)
+        //设置大小：宽度，高度
+        window!!.setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT)
+        //设置位置：底部显示
+        window!!.setGravity(Gravity.BOTTOM)
+
+        mBtncancle.onClick { dismiss() }
+
+    }
+
+}
---
+++ app/src/main/java/com/winzhibin/customdialog/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.winzhibin.customdialog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
+
+class MainActivity : AppCompatActivity() {
+
+    //初始化buttom
+   private val mBtn :Button by lazy {
+        btn
+    }
+
+    //初始化BottomUpDialog
+    private val mBottomUpDialog :BottomUpDialog by lazy {
+        BottomUpDialog(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mBtn.onClick {
+            //显示弹窗
+            mBottomUpDialog.show()
+        }
+    }
+}
